abf61dafd8bc08a221cb1a8533c18f3774094ce5
Kyunghwan <rudghks373@naver.com>
2020-12-21 11:18:03 +0900

    201 응답 받기

--- src/main/java/me/kyunghwan/springrestapi/events/Event.java
+++ src/main/java/me/kyunghwan/springrestapi/events/Event.java
@@ -9,11 +9,13 @@ import java.time.LocalDateTime;
 @Getter @Setter @EqualsAndHashCode(of = "id") //연관관계 묶음 NO! //엔티티 에는 @Data 어노테이션 NO!
 public class Event {
 
+    private Integer id;
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
     private LocalDateTime closeEnrollmentDateTime;
     private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
     private String location; // (optional) 이게 없으면 온라인 모임
     private int basePrice; // (optional)
     private int maxPrice; // (optional)
--- src/test/java/me/kyunghwan/springrestapi/events/EventControllerTests.java
+++ src/test/java/me/kyunghwan/springrestapi/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package me.kyunghwan.springrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -19,13 +24,32 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("Rest Api")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019,12,20,11,9))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019,12,20,11,10))
+                .beginEventDateTime(LocalDateTime.of(2019,12,20,13,10))
+                .endEventDateTime(LocalDateTime.of(2019,12,23,11,10))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("삼성역")
+                .build();
+
 
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated()); //is.("201")
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated()) //is.("201")
+                .andExpect(jsonPath("id").exists());
     }
 
 }
