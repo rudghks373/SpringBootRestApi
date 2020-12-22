5a4483d99e63eebaf4b0033541c8a768d18c0f2d
Kyunghwan <rudghks373@naver.com>
2020-12-22 11:29:08 +0900

    테스트용 DB와 설정 분리

--- src/test/java/me/kyunghwan/springrestapi/events/EventControllerTests.java
+++ src/test/java/me/kyunghwan/springrestapi/events/EventControllerTests.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfigureation.class)
+@ActiveProfiles("test")
 public class EventControllerTests {
 
     @Autowired
