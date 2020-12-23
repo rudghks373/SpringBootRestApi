6c4984a0c22c55f45360ff6c01f0e48de4bc0e82
Kyunghwan <rudghks373@naver.com>
2020-12-23 17:11:03 +0900

    스프링 시큐리티 폼 인증 설정

--- src/main/java/me/kyunghwan/springrestapi/config/AppConfig.java
+++ src/main/java/me/kyunghwan/springrestapi/config/AppConfig.java
@@ -1,10 +1,18 @@
 package me.kyunghwan.springrestapi.config;
 
+import me.kyunghwan.springrestapi.accounts.Account;
+import me.kyunghwan.springrestapi.accounts.AccountRole;
+import me.kyunghwan.springrestapi.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -17,6 +25,24 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account test = Account.builder()
+                        .email("test")
+                        .password("test")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(test);
+            }
+        };
     }
 
 }
