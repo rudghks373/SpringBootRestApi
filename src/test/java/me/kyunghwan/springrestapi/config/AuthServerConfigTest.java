119ff966e5909c843fc3668d602017b986c61f11
Kyunghwan <rudghks373@naver.com>
2020-12-24 10:07:56 +0900

    스프링 시큐리티 OAuth2 설정 (인증 서버 설정)

---
+++ src/test/java/me/kyunghwan/springrestapi/config/AuthServerConfigTest.java
@@ -0,0 +1,51 @@
+package me.kyunghwan.springrestapi.config;
+
+import me.kyunghwan.springrestapi.accounts.Account;
+import me.kyunghwan.springrestapi.accounts.AccountRole;
+import me.kyunghwan.springrestapi.accounts.AccountService;
+import me.kyunghwan.springrestapi.common.BaseControllerTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class AuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @DisplayName("인증 토큰을 발급 받는 테스트")
+    public void authToken() throws Exception {
+        String username = "ohtest";
+        String password = "pass";
+        Account testAccount = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountService.saveAccount(testAccount);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        this.mockMvc.perform(post("/oauth/token")
+                    .with(httpBasic(clientId, clientSecret))
+                    .param("username" , username)
+                    .param("password", password)
+                    .param("grant_type", "password"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists())
+        ;
+    }
+
+}
